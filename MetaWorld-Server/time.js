b8dfffad51e6a81438bf5e288cacbb77fc488164
Dylan <dbaker5525@gmail.com>
2024-10-17 09:07:00 -0400

    Fixing initialization bug with time.

--- MetaWorld-Server/time.js
+++ MetaWorld-Server/time.js
@@ -1,15 +1,20 @@
 module.exports = function(context, secondsPerDay, updateFrequency) {
     setInterval(async function() {
         await context.db.GetAllRows("time", (rows) => {
-            day = rows[0].day;
-            seconds = rows[0].seconds;
-
-            seconds += updateFrequency;
-            if (seconds > secondsPerDay) {
-                seconds -= secondsPerDay;
-                day += 1;
+            if (rows == null) {
+                day = 0;
+                seconds = 0;
             }
-            
+            else {
+                day = rows[0].day;
+                seconds = rows[0].seconds;
+
+                seconds += updateFrequency;
+                if (seconds > secondsPerDay) {
+                    seconds -= secondsPerDay;
+                    day += 1;
+                }
+            }
             context.db.UpdateInTable("time", { "day": day, "seconds": seconds });
         });
     }, updateFrequency * 1000);
